e0ed8518c73834b609ceaa78dc934419b2d5cf45
Lucas Delbel <lucdelbel@gmail.com>
2021-04-25 12:52:18 -0300

    Finish messages communication and displaying

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -36,6 +36,34 @@ class ConnectionsService {
 
     return connection;
   }
+
+  async findAllWithoutAdmin(){
+    const connections = await this.connectionsRepository.find({
+      where: { admin_id: null },
+      relations: ["user"]
+    })
+
+    return connections;
+  }
+
+  async findBySocketId(socket_id: string) {
+    const connection = await this.connectionsRepository.findOne({
+      socket_id,
+    })
+
+    return connection;
+  }
+
+  async updateAdminID(user_id: string, admin_id: string) {
+    this.connectionsRepository
+    .createQueryBuilder()
+    .update(Connection)
+    .set({ admin_id })
+    .where("user_id = :user_id", {
+      user_id,
+    })
+    .execute();
+  }
 }
 
 export { ConnectionsService }
--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -33,9 +33,34 @@ io.on("connect", (socket: Socket) => {
       await connectionsService.create(connection);
     }
 
-    await messagesService.create({ 
+    await messagesService.create({
       text,
-      user_id
+      user_id,
     });
+
+    const allMessages = await messagesService.listByUser(user_id);
+
+    socket.emit("client_list_all_messages", allMessages);
+
+    const allUsers = connectionsService.findAllWithoutAdmin();
+    io.emit("admin_list_all_users", allUsers);
+  });
+
+  socket.on("client_send_to_admin", async (params) => {
+    const { text, socket_admin_id } = params;
+
+    const socket_id = socket.id;
+
+    const { user_id } = await connectionsService.findBySocketId(socket.id);
+
+    const message = await messagesService.create({
+      text,
+      user_id,
+    });
+
+    io.to(socket_admin_id).emit("admin_receive_message", {
+      message,
+      socket_id
+    })
   });
 });
